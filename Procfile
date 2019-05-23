cf356767d4e065171d2bc9dbfe0cb9fee20e58f1
Javier Castro <javier.castro@mercadolibre.com>
2019-05-23 19:14:33 -0300

    add front

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node src/server/app.js
+web: npm run server
--- src/server/app.js
+++ src/server/app.js
@@ -1,16 +1,19 @@
 require('dotenv').config()
+const path = require('path');
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var urlDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/uade_sem2_shart'
 var mongoose = require('mongoose')
-var port = process.env.PORT || 9090
+var port = process.env.PORT || 8080
 var JWT = require('./helpers/hash')
 var LogMiddleware = require('./helpers/log')
 
 app.use(LogMiddleware.logger_middleware)
 // Auth middleware
 // app.use(JWT.jwt_user)
+
+app.use(express.static(path.join(__dirname, '../../dist')));
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -25,6 +28,20 @@ mongoose.connect(urlDB)
 app.get('/api', function (req, res) {
   res.send({ result: 'happy to be here' })
 })
+
+
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
 app.listen(port, function () {
   console.log('app listening on port ' + port)
