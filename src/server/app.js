7e00a8b6dafd15f2f6d63a9fd9afd29d8c4c8e32
Javier Castro <javier.castro@mercadolibre.com>
2019-05-29 23:50:21 -0300

    fix

--- src/server/app.js
+++ src/server/app.js
@@ -1,52 +1,55 @@
-require('dotenv').config()
+require('dotenv').config();
 const path = require('path');
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-var urlDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/uade_sem2_shart'
-var mongoose = require('mongoose')
-var port = process.env.PORT || 80
-// var JWT = require('./helpers/hash')
-var LogMiddleware = require('./helpers/log')
+const express = require('express');
 
-app.use(LogMiddleware.logger_middleware)
+const app = express();
+const bodyParser = require('body-parser');
+
+const urlDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/uade_sem2_shart';
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 80;
+// var JWT = require('./helpers/hash')
+const LogMiddleware = require('./helpers/log');
+
+app.use(LogMiddleware.logger_middleware);
 // Auth middleware
 // app.use(JWT.jwt_user)
 
 app.use(express.static(path.join(__dirname, '../../dist')));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
-}))
+}));
 
 // app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.use('/api/users', require('./controllers/users'))
-app.use('/api/clients', require('./controllers/clients'))
-app.use('/api/mes', require('./controllers/mes'))
-app.use('/api/auth', require('./controllers/auth'))
-mongoose.connect(urlDB)
+app.use('/api/users', require('./controllers/users'));
+app.use('/api/clients', require('./controllers/clients'));
+app.use('/api/mes', require('./controllers/mes'));
+app.use('/api/auth', require('./controllers/auth'));
 
-app.get('/api', function (req, res) {
-  res.send({ result: 'happy to be here' })
-})
+mongoose.connect(urlDB);
 
+app.get('/api', (req, res) => {
+  res.send({ result: 'happy to be here' });
+});
 
 
 // An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
+app.get('/api/getList', (req, res) => {
+  const list = ['item1', 'item2', 'item3', 'item1', 'item2', 'item3', 'item1', 'item2', 'item3'];
+  res.json(list);
+  console.log('Sent list of items');
 });
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}../../../dist/index.html`));
 });
 
-app.listen(port, function () {
-  console.log('app listening on port ' + port)
-})
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
 
-module.exports = app
+module.exports = app;
