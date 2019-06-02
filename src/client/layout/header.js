5bdbea7b3f25d095e2be45b00bfa3c54c0ec645a
Erica Nuñez <erikannunez@gmail.com>
2019-06-02 13:49:45 -0300

    Mejoras header y Update Material-UI
    Mejoras visuales en título, logo, menu, inicio de sesión...
    Update de la versión de MaterialUI de 3.9.3 a 4.0.0

--- src/client/layout/header.js
+++ src/client/layout/header.js
@@ -1,16 +1,37 @@
 import React, { useContext } from 'react';
-
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
-import AppContext from '../context';
-
+import Button from '@material-ui/core/Button';
 import './header.scss';
+import AppContext from '../context';
+import Logo from '../assets/shart-64x64.png';
+
+const useStyles = makeStyles(theme => ({
+  hLogo: {
+    marginRight: 10,
+    width: 32,
+    height: 32
+  },
+  login: {
+    float: 'right'
+  },
+  title: {
+    backgroundImage: `url(${Logo})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '32px 32px',
+    color: '#FFF',
+    paddingLeft: '42px',
+    textAlign: 'left',
+    flexGrow: 1
+  }
+}));
 
 const Header = () => {
+  const classes = useStyles();
   const { toggleDrawer } = useContext(AppContext);
 
   return (
@@ -19,9 +40,12 @@ const Header = () => {
         <IconButton edge="start" color="inherit" aria-label="Menu" onClick={toggleDrawer(true)}>
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" className="header_title">
-          Shart
+        <Typography pl={10} variant="h6" className={classes.title}>
+          SHART
         </Typography>
+        <Button className={classes.login} color="inherit">
+          <Typography>Iniciar Sesión</Typography>
+        </Button>
       </Toolbar>
     </AppBar>
   );
