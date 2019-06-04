3c1a24d38dc8e3d41439300e7163864d39243d81
FlorOtero <otero.florencia96@gmail.com>
2019-06-03 22:36:01 -0300

    List projects and define action depending of owner

--- src/client/components/projectItem/index.js
+++ src/client/components/projectItem/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-
 import { Link } from 'react-router-dom';
-
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -11,39 +9,48 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
+import AppContext from '../../context';
 
-
-const ProjectItem = ({ id, name, description, category, tags }) => (
-  <Card className="project-item">
-    <Link to={`/projects/${id}`}>
-      <CardActionArea>
-        <CardMedia
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography color="textSecondary" gutterBottom>
-            {category}
-          </Typography>
-          <Typography gutterBottom variant="h5" component="h2">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {description}
-          </Typography>
-        </CardContent>
-        <CardContent>
-          {tags.map(tag => <Chip label={tag} />)}
-        </CardContent>
-      </CardActionArea>
-    </Link>
-    <CardActions>
-      <Button size="small" color="primary">
-        Formar parte
-      </Button>
-    </CardActions>
-  </Card>
-);
+const ProjectItem = ({ id, name, description, category, tags, owner }) => {
+  const { user } = useContext(AppContext);
+  return (
+    <Card className="project-item">
+      <Link to={`/projects/${id}`}>
+        <CardActionArea>
+          <CardMedia
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              {category}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="h2">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {description}
+            </Typography>
+          </CardContent>
+          <CardContent>
+            {tags.map(tag => <Chip label={tag} />)}
+          </CardContent>
+        </CardActionArea>
+      </Link>
+      <CardActions>
+      { owner.id === user.id ? 
+        <Button size="small" color="primary">
+          Ver mi proyecto
+        </Button>
+      :
+        <Button size="small" color="primary">
+          Formar parte
+        </Button>
+      }
+      </CardActions>
+    </Card>
+  )
+}
 
 ProjectItem.propTypes = {
   id: PropTypes.number.isRequired,
