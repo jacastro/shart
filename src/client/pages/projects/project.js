9a7f9e63cf6ccfe3aa7a2d766e1d767f647df55b
Erica Nuñez <erikannunez@gmail.com>
2019-06-06 01:11:02 -0300

    project view

--- src/client/pages/projects/project.js
+++ src/client/pages/projects/project.js
@@ -1,4 +1,9 @@
 import React, { useContext } from 'react';
+import keyIndex from 'react-key-index';
+import Moment from 'react-moment';
+import 'moment-timezone';
+
+import Button from '@material-ui/core/Button';
 import Chip from '@material-ui/core/Chip';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -6,13 +11,44 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import IconButton from '@material-ui/core/IconButton';
+import PlaceIcon from '@material-ui/icons/Place';
+import ShareIcon from '@material-ui/icons/Share';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 import Typography from '@material-ui/core/Typography';
-
 import AppContext from '../../context';
+
+const phases = [{
+  id: 'init',
+  name: 'Inicio',
+}, {
+  id: 'design',
+  name: 'Diseño',
+}, {
+  id: 'development',
+  name: 'Desarrollo',
+}, {
+  id: 'decoration',
+  name: 'Decoración',
+}, {
+  id: 'ship',
+  name: 'Envío',
+}, {
+  id: 'final',
+  name: 'Finalizado',
+}];
 
 class Project extends React.Component {
   state = {
-    project: null,
+    project: null
   };
 
   componentDidMount() {
@@ -28,20 +64,94 @@ class Project extends React.Component {
       <React.Fragment>
         {project == null ? <CircularProgress /> : (
           <Card className="card-project">
-            <CardHeader title={project.name} subheader={`Categoría: ${project.category}`} />
+            <CardHeader title={project.name} subheader={`Liderado por ${project.project_leader.full_name}`} />
             {project.images.length > 0
-            && <CardMedia className="card-media" image={project.images[0]} />
+            && <CardMedia className="card-project-media" image={project.images[0]} />
             }
             <CardContent>
-              <Typography>{project.description}</Typography>
-              {project.tags.length > 0
-                && <Typography variant="overline" gutterBottom>Tags: </Typography>
-              }
-              {project.tags.map((tag, index) => (
-                <Chip key={tag} className="card-tags" label={tag} component="a" href={`/tag/${tag}`} />
-              ))}
+              <Grid container spacing={5}>
+                <Grid item xs={8} className="card-project-meta">
+                  <div>
+                    <Typography variant="body2" display="block">{project.description}</Typography>
+                  </div>
+                  <div>
+                    <Typography variant="overline">Ubicación:</Typography>
+                    <Chip className="card-project-tags" label={project.region} icon={<PlaceIcon />} component="a" href={`/places/${project.region}`} />
+                  </div>
+                </Grid>
+                <Grid item xs={4} className="card-project-meta">
+                  <div>
+                    <Typography variant="overline" gutterBottom>Categoría: </Typography>
+                    <Chip color="primary" key={project.category} label={project.category} component="a" href={`/categories/${project.category}`} />
+                  </div>
+                  <div>
+                    {project.tags.length > 0
+                    && <Typography variant="overline" gutterBottom>Tags: </Typography>
+                  }
+                    {project.tags.map((tag, index) => (
+                      <Chip color="secondary" key={tag} className="card-project-tags" label={tag} component="a" href={`/tag/${tag}`} />
+                    ))}
+                  </div>
+                  {project.images.length > 1
+                    && (
+                      <div>
+                        <Typography variant="overline" gutterBottom>Imágenes del Proyecto:</Typography>
+                        <div className="card-project-gallery-root">
+                          <GridList cellHeight={100} className="card-project-gallery" cols={3}>
+                            {project.images.map((image, index) => (
+                              <GridListTile key={Math.random()} cols={Math.floor(Math.random() * 3) + 1}>
+                                <img src={image} alt={image} />
+                              </GridListTile>
+                            ))}
+                          </GridList>
+                        </div>
+                      </div>
+                    )
+                  }
+                </Grid>
+                <Grid item xs={12}>
+                  <Stepper>
+                    {phases.map((phase, index) => (
+                      <Step key={phase.id} active={project.current_phase === phase.id}>
+                        <StepLabel key={phase.id}>
+                          {phase.name.toUpperCase()}
+                          {phase.id === 'init'
+                        && (
+                        <Typography display="block" variant="overline">
+                          <Moment format="DD/MM/YYYY">{project.start_date}</Moment>
+                        </Typography>
+                        )}
+                          {phase.id === 'final'
+                        && (
+                        <Typography display="block" variant="overline">
+                          <Moment format="DD/MM/YYYY">{project.end_date}</Moment>
+                        </Typography>
+                        )}
+                        </StepLabel>
+                      </Step>
+                    ))}
+                  </Stepper>
+                </Grid>
+                <Grid item xs={12} />
+              </Grid>
             </CardContent>
-            <CardActions />
+            <Divider variant="middle" />
+            <CardActions>
+              <IconButton className="card-project-action" aria-label="Add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton className="card-project-action" aria-label="Compartir">
+                <ShareIcon />
+              </IconButton>
+              {project.need_collaborations
+              && (
+              <Button href="#" className="card-project-apply" color="primary" variant="outlined">
+                <AddCircleIcon />
+Postularme
+              </Button>
+              )
+              }
+            </CardActions>
           </Card>
         )}
       </React.Fragment>
