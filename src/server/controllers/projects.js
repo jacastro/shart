9a97eead79439659a2f38cdba6b386b13b909309
matt <matiasberrueta@gmail.com>
2019-06-26 22:25:43 -0300

    Refactor & sort

--- src/server/controllers/projects.js
+++ src/server/controllers/projects.js
@@ -8,165 +8,35 @@ const Users = require('../models/users');
 const Mes = require('../models/mes');
 const Projects = require('../models/projects');
 
-// Get all my projects /api/users/:user_id/projects
-router.get('/:user_id/projects/', (req, res) => {
-  if (!req.params.user_id) {
-    res.status(404).json({ errors: 'user not found' });
-    return;
+function shuffle(array) {
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
 
-  Users.find({ id: req.params.user_id })
-    .then((user) => {
-      if (!user || !user[0]._id) {
-        res.status(404).json({ errors: 'user not found' });
-        return;
-      }
-      const id = user[0]._id;
+  return array;
+}
 
-      const nameFilter = new RegExp(`.*${req.query.name || ''}.*`);
-      const descFilter = new RegExp(`.*${req.query.description || ''}.*`);
-      const catFilter = new RegExp(`.*${req.query.category || ''}.*`);
-      const regionFilter = new RegExp(`.*${req.query.region || ''}.*`);
-      const filter = {
-        owner: id,
-        name: { $regex: nameFilter, $options: 'i' },
-        category: { $regex: catFilter, $options: 'i' },
-        region: { $regex: regionFilter, $options: 'i' },
-        description: { $regex: descFilter, $options: 'i' }
-      };
-      if (req.query.tags) {
-        filter.tags = { $in: req.query.tags.split(',') };
-      }
-      if (req.query.only_need_collaboration) {
-        filter.need_collaborations = true;
-        filter.finished = false;
-        filter['phases.tasks.collaborator'] = null;
-      }
-
-      Projects.find(filter, '-_id -__v -rating_sum -rating_count')
-        .populate('owner', '-_id -__v')
-        .populate({
-          path: 'project_leader',
-          select: '-_id -__v',
-          populate: [{
-            path: 'user',
-            select: '-_id -__v'
-          }]
-        })
-        .populate({
-          path: 'phases.tasks.collaborator',
-          select: '-_id -__v',
-          populate: [{
-            path: 'user',
-            select: '-_id -__v',
-          }]
-        })
-        .populate({
-          path: 'postulants.collaborator',
-          select: '-_id -__v',
-          populate: [{
-            path: 'user',
-            select: '-_id -__v'
-          }]
-        })
-
-        .then((projects) => {
-          res.status(200).json({ projects });
-        })
-        .catch((err) => {
-          res.status(500).json({ message: 'Something went wrong', error: err });
-        });
-    })
-    .catch((err) => {
-      res.status(500).json({ message: 'Something went wrong', error: err });
-    });
-});
-
-// Get all projects I'm working on /api/users/:user_id/working-projects
-router.get('/:user_id/working-projects/', (req, res) => {
-  if (!req.params.user_id) {
-    res.status(404).json({ errors: 'user not found' });
-    return;
-  }
-
-  Users.findOne({ id: req.params.user_id })
-    .populate('me')
-    .then((user) => {
-      if (!user) {
-        res.status(404).json({ errors: 'user not found' });
-        return;
-      }
-      if (!user.me) {
-        res.status(404).json({ errors: 'user is not ME' });
-        return;
-      }
-
-      const nameFilter = new RegExp(`.*${req.query.name || ''}.*`);
-      const descFilter = new RegExp(`.*${req.query.description || ''}.*`);
-      const catFilter = new RegExp(`.*${req.query.category || ''}.*`);
-      const regionFilter = new RegExp(`.*${req.query.region || ''}.*`);
-      const filter = {
-        'phases.tasks.collaborator': user.me,
-        name: { $regex: nameFilter, $options: 'i' },
-        category: { $regex: catFilter, $options: 'i' },
-        region: { $regex: regionFilter, $options: 'i' },
-        description: { $regex: descFilter, $options: 'i' },
-        finished: false
-      };
-      if (req.query.tags) {
-        filter.tags = { $in: req.query.tags.split(',') };
-      }
-
-      Projects.find(filter, '-_id -__v -rating_sum -rating_count')
-        .populate('owner', '-_id -__v')
-        .populate({
-          path: 'project_leader',
-          select: '-_id -__v',
-          populate: [{
-            path: 'user',
-            select: '-_id -__v'
-          }]
-        })
-        .populate({
-          path: 'phases.tasks.collaborator',
-          select: '-_id -__v',
-          populate: [{
-            path: 'user',
-            select: '-_id -__v',
-          }]
-        })
-        .populate({
-          path: 'postulants.collaborator',
-          select: '-_id -__v',
-          populate: [{
-            path: 'user',
-            select: '-_id -__v'
-          }]
-        })
-
-        .then((projects) => {
-          res.status(200).json({ projects });
-        })
-        .catch((err) => {
-          res.status(500).json({ message: 'Something went wrong', error: err });
-        });
-    })
-    .catch((err) => {
-      res.status(500).json({ message: 'Something went wrong', error: err });
-    });
-});
-
-router.get('/', (req, res) => {
+function findProjects(req, res, filter = {}, sort) {
   const nameFilter = new RegExp(`.*${req.query.name || ''}.*`);
   const descFilter = new RegExp(`.*${req.query.description || ''}.*`);
   const catFilter = new RegExp(`.*${req.query.category || ''}.*`);
   const regionFilter = new RegExp(`.*${req.query.region || ''}.*`);
-  const filter = {
-    name: { $regex: nameFilter, $options: 'i' },
-    category: { $regex: catFilter, $options: 'i' },
-    region: { $regex: regionFilter, $options: 'i' },
-    description: { $regex: descFilter, $options: 'i' }
-  };
+  filter.name = { $regex: nameFilter, $options: 'i' };
+  filter.category = { $regex: catFilter, $options: 'i' };
+  filter.region = { $regex: regionFilter, $options: 'i' };
+  filter.description = { $regex: descFilter, $options: 'i' };
   if (req.query.tags) {
     filter.tags = { $in: req.query.tags.split(',') };
   }
@@ -176,7 +46,8 @@ router.get('/', (req, res) => {
     filter['phases.tasks.collaborator'] = null;
   }
 
-  Projects.find(filter, '-_id -__v -rating_sum -rating_count')
+  const orderBy = sort || {};
+  Projects.find(filter, '-_id -__v -rating_sum -rating_count', orderBy)
     .populate('owner', '-_id -__v')
     .populate({
       path: 'project_leader',
@@ -203,12 +74,131 @@ router.get('/', (req, res) => {
       }]
     })
     .then((projects) => {
+      if (!sort) {
+        res.status(200).json(shuffle(projects));
+        return;
+      }
+
       res.status(200).json({ projects });
     })
     .catch((err) => {
       res.status(500).json({ message: 'Something went wrong', error: err });
     });
+}
+
+// Get all my projects /api/users/:user_id/projects
+router.get('/:user_id/projects/', (req, res) => {
+  if (!req.params.user_id) {
+    res.status(404).json({ errors: 'user not found' });
+    return;
+  }
+
+  Users.find({ id: req.params.user_id })
+    .then((user) => {
+      if (!user || !user[0]._id) {
+        res.status(404).json({ errors: 'user not found' });
+        return;
+      }
+      const id = user[0]._id;
+
+      const filter = {
+        owner: id
+      };
+
+      findProjects(req, res, filter);
+    });
 });
+
+// Get all projects I'm working on /api/users/:user_id/working-projects
+router.get('/:user_id/working-projects/', (req, res) => {
+  if (!req.params.user_id) {
+    res.status(404).json({ errors: 'user not found' });
+    return;
+  }
+
+  Users.findOne({ id: req.params.user_id })
+    .populate('me')
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ errors: 'user not found' });
+        return;
+      }
+      if (!user.me) {
+        res.status(404).json({ errors: 'user is not ME' });
+        return;
+      }
+
+      const filter = {
+        'phases.tasks.collaborator': user.me,
+        finished: false
+      };
+
+      const orderBy = { sort: { start_date: 'desc' } };
+      findProjects(req, res, filter, orderBy);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Something went wrong', error: err });
+    });
+});
+
+// Get all
+router.get('/', (req, res) => {
+  findProjects(req, res);
+});
+
+// router.get('/', (req, res) => {
+//   const nameFilter = new RegExp(`.*${req.query.name || ''}.*`);
+//   const descFilter = new RegExp(`.*${req.query.description || ''}.*`);
+//   const catFilter = new RegExp(`.*${req.query.category || ''}.*`);
+//   const regionFilter = new RegExp(`.*${req.query.region || ''}.*`);
+//   const filter = {
+//     name: { $regex: nameFilter, $options: 'i' },
+//     category: { $regex: catFilter, $options: 'i' },
+//     region: { $regex: regionFilter, $options: 'i' },
+//     description: { $regex: descFilter, $options: 'i' }
+//   };
+//   if (req.query.tags) {
+//     filter.tags = { $in: req.query.tags.split(',') };
+//   }
+//   if (req.query.only_need_collaboration) {
+//     filter.need_collaborations = true;
+//     filter.finished = false;
+//     filter['phases.tasks.collaborator'] = null;
+//   }
+
+//   Projects.find(filter, '-_id -__v -rating_sum -rating_count')
+//     .populate('owner', '-_id -__v')
+//     .populate({
+//       path: 'project_leader',
+//       select: '-_id -__v',
+//       populate: [{
+//         path: 'user',
+//         select: '-_id -__v'
+//       }]
+//     })
+//     .populate({
+//       path: 'phases.tasks.collaborator',
+//       select: '-_id -__v',
+//       populate: [{
+//         path: 'user',
+//         select: '-_id -__v',
+//       }]
+//     })
+//     .populate({
+//       path: 'postulants.collaborator',
+//       select: '-_id -__v',
+//       populate: [{
+//         path: 'user',
+//         select: '-_id -__v'
+//       }]
+//     })
+//     .then((projects) => {
+//       res.status(200).json({ projects });
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ message: 'Something went wrong', error: err });
+//     });
+// });
 
 router.get('/:id', (req, res) => {
   if (!req.params.id) {
